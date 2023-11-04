0a3e518e411ccf2a16ae3c002b2364c3f3cbf423
Sebastian barrera <sebasbh2008@hotmail.com>
2023-11-03 19:34:08 -0500

    menu burger

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import './header.scss';
 import logo from '../../assets/images/logo.png'
 import expand from '../../assets/images/expand.png'
 
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false)
     const navigate = useNavigate()
+    const toggleMenu = () => {
+        setOpenMenu(true)
+        console.log("funciona");
+    }
     return (
         <header className='header'>
             <figure className='header__figure'>
@@ -36,20 +41,36 @@ const Header = () => {
                     <li className='header__ul--li'><a href="" className='nav__ancora'>Contacto</a></li>
                 </ul>
             </nav>
+            <svg xmlns="http://www.w3.org/2000/svg" height="44" viewBox="0 -960 960 960" width="44" fill='#BB1F25' onClick={toggleMenu}><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" className='burger__icon' /></svg>
 
-            <svg xmlns="http://www.w3.org/2000/svg" height="44" viewBox="0 -960 960 960" width="44" fill='#BB1F25'><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" className='burger__icon' /></svg>
-
-            <div className="menu">
-                <h2 className='mensaje'>Este es tu mensaje visible</h2>
-                <ul>
-                    <li>nada</li>
-                    <li>dfdsfdsf</li>
-                    <li>dfdsfds</li>
-                    <li>dsfdsf</li>
-                    <li>dfdsfdsf</li>
+            <section className={` menu ${openMenu? "active" : ""}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="44" viewBox="0 -960 960 960" width="44" fill='#BB1F25' className='close' onClick={() => setOpenMenu(false)} ><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+                <h2 className='title__burger'>Menú</h2>
+                <ul className='ul__burger'>
+                    <li><a href="" className='li__burger'>Carpinteria</a></li>
+                    <li><a href="" className='li__burger'>Electrico</a></li>
+                    <li><a href="" className='li__burger'>Construcción</a></li>
+                    <li><a href="" className='li__burger'>Cotización</a></li>
+                    <li><a href="" className='li__burger'>Trabaja con nosotros</a></li>
+                    <li><a href="" className='li__burger'>Quienes somos</a></li>
+                    <li><a href="" className='li__burger'>Donde estamos ubicados</a></li>
+                    <li><a href="" className='li__burger'>Contacto</a></li>
                 </ul>
-            </div>
+                <figure className="redes">
+                    <a href="">
+                        <img src="https://cdn-icons-png.flaticon.com/512/1384/1384095.png" className='wpp__redes' alt="Whatsaap icono" />
+                    </a>
+                    <a href="">
+                        <img src="https://cdn-icons-png.flaticon.com/512/81/81341.png" className='face__redes' alt="facebook" />
+                    </a>
+                    <a href="">
+                        <img src="https://cdn-icons-png.flaticon.com/512/1384/1384015.png" className='ig__redes' alt="instagram" />
+                    </a>
+                </figure>
+                <span className='span__burger'>Acabados y estilos en madera</span>
+            </section>
         </header>
+
     )
 }
 
